ffa15de7fe91e0db346415d1a4349a788945ed41
mclausen <mocl@itu.dk>
2025-05-12 15:24:22 +0200

    working registered function call

--- main.c
+++ main.c
@@ -4,13 +4,21 @@
 #include "ubpf.h"
 #include "verified_functions.h"
 
+struct mem_range *obtain(struct request *reqs, int num){
+    struct mem_range *dummy = malloc(sizeof(struct mem_range));
+    dummy->ptr = (char*)malloc(12);
+    dummy->length = 12;
+    return dummy;
+};
+
 int main(){
     // Setup Ubpf VM
     struct ubpf_vm* vm = ubpf_create();
+    ubpf_register(vm, 2, "obtain", &obtain);
     int res = ubpf_register(vm, 1, "aggregate_by_pos", &aggregate_by_pos);
     printf("Registering code: %s\n", res ? "failed" : "succeeded");
     // Load ebpf code
-    FILE *fptr = fopen("programs/test.o", "rb");
+    FILE *fptr = fopen("programs/aggregate.o", "rb");
     fseek(fptr, 0L, SEEK_END);
     int sz = ftell(fptr);
     rewind(fptr);
@@ -28,17 +36,18 @@ int main(){
     };
     int dummy_data[] = {1,2,3,0,2};
     int data_size = sizeof(struct layout)+sizeof(dummy_data);
-    struct layout *input = (struct layout*)malloc(1024);
+    struct layout *input = (struct layout*)malloc(sizeof(struct layout)+data_size);
     input -> element_size = 3*sizeof(int);
-    input -> index_size = 2*sizeof(int);
+    input -> index_size = 2*sizeof(uint32_t);
     void *data = input->dynamic;
     memcpy(input->dynamic, dummy_data, sizeof(dummy_data));
     // Execute program
     uint64_t result;
     void *shared = malloc(1024);
     printf("Before crash\n");
-    ubpf_exec(vm, input, 1024, &result, shared, 1024);
-    printf("Execution result: %ld\n", result);
+    ubpf_exec(vm, input, sizeof(struct layout)+data_size, &result, shared, 1024);
+    printf("Execution result: %d\n", input->result);
+    printf("Length of shared region: %d\n", result);
     // Clean up
     free(shared);
     free(input);
--- programs/test.c
+++ programs/test.c
@@ -1,7 +1,11 @@
-#include "../external/delilah/include/verified_functions.h"
-int prog(void* ctx){
-    int data[] = {1,2,3};
-    uint32_t idx[] = {0,2};
-    aggregate_by_pos(data, 3, idx, 2);
-    return 28;
+// #include "../external/delilah/include/verified_functions.h"
+struct layout {
+        int element_size;
+        int index_size;
+        int result;
+        char dynamic[];
+    };
+int prog(void* ctx, int ctx_len, void* cache, int cache_len){
+    struct layout *info = (struct layout *)ctx;
+    return aggregate_by_pos(info->dynamic,3,info->dynamic+3*sizeof(int),2);
 }
